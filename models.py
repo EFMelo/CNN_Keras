ad79f0860f50e96240d140d4198cb3ea5f0937e5
Edvaldo Melo <emeloppgi@gmail.com>
2020-10-05 14:26:30 -0300

    Adding the cnn configuration for cifar10

--- models.py
+++ models.py
@@ -1,5 +1,7 @@
 from keras.models import Sequential
-from keras.layers import Dense, Conv2D, MaxPooling2D, Flatten, Dropout
+from keras.layers import Dense, Conv2D, MaxPooling2D, Flatten, Dropout, Activation, ELU
+from keras.optimizers import Adam
+from keras.layers.normalization import BatchNormalization
 
 class BartAndHomerModel:
 
@@ -58,5 +60,48 @@ class MnistModel:
 
         # Configuring the network
         model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+        return model
+
+
+class Cifar10Model:
+    
+    @classmethod
+    def build(cls):
+        
+        model = Sequential()
+        
+        # conv - 64
+        model.add(Conv2D(64, (3,3), input_shape=(32, 32, 3), padding='same'))
+        model.add(BatchNormalization())
+        model.add(Activation('relu'))
+        model.add(Conv2D(64, (3,3), padding='same'))
+        model.add(BatchNormalization())
+        model.add(Activation('relu'))
+        model.add(MaxPooling2D((2,2)))
+        
+        # conv - 128
+        model.add(Conv2D(128, (3,3), padding='same'))
+        model.add(BatchNormalization())
+        model.add(Activation('relu'))
+        model.add(Conv2D(128, (3,3), padding='same'))
+        model.add(BatchNormalization())
+        model.add(Activation('relu'))
+        model.add(MaxPooling2D((2,2)))
+        
+        # Flatening
+        model.add(Flatten())
+
+        # Dense layers
+        model.add(Dense(units=31))
+        model.add(ELU())
+        model.add(Dropout(0.5))
+        model.add(Dense(units=31))
+        model.add(ELU())
+        model.add(Dropout(0.5))
+        model.add(Dense(10, activation='softmax'))
+        
+        # Configuring the network
+        model.compile(optimizer=Adam(lr=1e-4), loss='categorical_crossentropy', metrics=['accuracy'])
 
         return model
